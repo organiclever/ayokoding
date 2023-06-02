c3ba501ff7790d00d88aac2c677d996aec77cdbb
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-02 07:43:28 +0700

    test formatter

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 export default {
-  '*.{js,jsx,ts,tsx}': ['npx prettier --write'],
+  '*.{js,jsx,ts,tsx,mjs}': ['npx prettier --write'],
   '*,{clj,cljc,cljs,edn}': (files) => {
     console.log(files);
     return `clj -Tcljfmt fix '{:paths [${files.join(', ')}]}'
