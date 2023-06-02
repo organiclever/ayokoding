b32dc5bc8dd14d142c70b75a273d16cd097ba456
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-02 10:35:16 +0700

    [clojure-cookbook] data types basic

---
+++ contents/clojure-cookbook/src/data_types_basic/core.clj
@@ -0,0 +1,31 @@
+(ns data-types-basic.core)
+
+;; Integers
+42
+;; => 42
+;; Float
+3.14
+;; => 3.14
+
+;; Booleans
+true
+;; => true
+false
+;; => false
+
+;; Character
+\a
+;; => \a
+\b
+;; => \b
+\
+;; => \space
+
+;; Strings
+"abcdef123456"
+;; => "abcdef123456"
+
+;; Keywords
+:my-keyword
+;; => :my-keyword
+
