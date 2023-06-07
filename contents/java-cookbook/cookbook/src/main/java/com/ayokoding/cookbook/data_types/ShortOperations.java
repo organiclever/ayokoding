14455be960edb73ddf7ade2b11f6bfeb3057ca0c
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-08 06:48:00 +0700

    [java-cookbook] Add short Operations

---
+++ contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/ShortOperations.java
@@ -0,0 +1,113 @@
+package com.ayokoding.cookbook.data_types;
+
+public class ShortOperations {
+  public static void main(String[] args) {
+
+    // Arithmetic Operations
+    short a = 1000;
+    short b = 2000;
+    short c;
+
+    c = (short) (a + b);
+    System.out.println(c); // 3000
+
+    c = (short) (b - a);
+    System.out.println(c); // 1000
+
+    c = (short) (a * b);
+    System.out.println(c); // -31616 (overflow occurred)
+
+    c = (short) (b / a);
+    System.out.println(c); // 2
+
+    c = (short) (b % a);
+    System.out.println(c); // 0
+
+    // Relational Operations
+    System.out.println(a == b); // false
+    System.out.println(a != b); // true
+    System.out.println(a > b); // false
+    System.out.println(a < b); // true
+    System.out.println(a >= b); // false
+    System.out.println(a <= b); // true
+
+    // Bitwise Operations
+    c = (short) (a & b);
+    System.out.println(c); // 960
+
+    c = (short) (a | b);
+    System.out.println(c); // 2040
+
+    c = (short) (a ^ b);
+    System.out.println(c); // 1080
+
+    c = (short) ~a;
+    System.out.println(c); // -1001
+
+    c = (short) (a << 2);
+    System.out.println(c); // 4000
+
+    c = (short) (a >> 2);
+    System.out.println(c); // 250
+
+    c = (short) (a >>> 2);
+    System.out.println(c); // 250
+
+    // Assignment Operations
+    a = 1000;
+    System.out.println(a); // 1000
+
+    a += 200;
+    System.out.println(a); // 1200
+
+    a -= 200;
+    System.out.println(a); // 1000
+
+    a *= 2;
+    System.out.println(a); // 2000
+
+    a /= 2;
+    System.out.println(a); // 1000
+
+    a %= 200;
+    System.out.println(a); // 0
+
+    a = 1000;
+    a &= 200;
+    System.out.println(a); // 200
+
+    a = 1000;
+    a |= 200;
+    System.out.println(a); // 1000
+
+    a = 1000;
+    a ^= 200;
+    System.out.println(a); // 800
+
+    a = 1000;
+    a <<= 2;
+    System.out.println(a); // 4000
+
+    a = 1000;
+    a >>= 2;
+    System.out.println(a); // 250
+
+    a = 1000;
+    a >>>= 2;
+    System.out.println(a); // 250
+
+    // Unary Operations
+    a = 1000;
+    c = (short) +a;
+    System.out.println(c); // 1000
+
+    c = (short) -a;
+    System.out.println(c); // -1000
+
+    a++;
+    System.out.println(a); // 1001
+
+    a--;
+    System.out.println(a); // 1000
+  }
+}
