265435104cc320090384ce4008d777ee106256d8
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-07 07:17:01 +0700

    [java-cookbook] Update Primitive Data Types

--- contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/Primitive.java
+++ contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/Primitive.java
@@ -4,21 +4,24 @@ public class Primitive {
 
   public static void main(String[] args) {
     byte b = 100;
-    short s = 1000;
-    int i = 100000;
-    long l = 1000000000L;
-    float f = 3.14f;
-    double d = 3.141592653589793;
-    char c = 'A';
-    boolean bool = true;
-
     System.out.println(b); // 100
+
+    short s = 1000;
     System.out.println(s); // 1000
+    int i = 100000;
     System.out.println(i); // 100000
+    long l = 1000000000L;
     System.out.println(l); // 1000000000
+
+    float f = 3.14f;
     System.out.println(f); // 3.14
+    double d = 3.141592653589793;
     System.out.println(d); // 3.141592653589793
+
+    char c = 'A';
     System.out.println(c); // A
+
+    boolean bool = true;
     System.out.println(bool); // true
   }
 }
