238d3af8ec29ed1ace3b668d2b7cc243d71715bb
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-08 07:10:09 +0700

    [java-cookbook] add primite float and double operations

--- contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/PrimitiveDoubleOperations.java
+++ contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/PrimitiveDoubleOperations.java
@@ -1,5 +1,60 @@
 package com.ayokoding.cookbook.data_types;
 
 public class PrimitiveDoubleOperations {
+  public static void main(String[] args) {
+    // Arithmetic Operations
+    double a = 10.0;
+    double b = 20.0;
+    double c;
 
+    c = a + b;
+    System.out.println(c); // 30.0
+
+    c = b - a;
+    System.out.println(c); // 10.0
+
+    c = a * b;
+    System.out.println(c); // 200.0
+
+    c = b / a;
+    System.out.println(c); // 2.0
+
+    c = b % a;
+    System.out.println(c); // 0.0
+
+    // Relational Operations
+    System.out.println(a == b); // false
+    System.out.println(a != b); // true
+    System.out.println(a > b); // false
+    System.out.println(a < b); // true
+    System.out.println(a >= b); // false
+    System.out.println(a <= b); // true
+
+    // Assignment Operations
+    a = 10.0;
+    System.out.println(a); // 10.0
+
+    a += 2.0;
+    System.out.println(a); // 12.0
+
+    a -= 2.0;
+    System.out.println(a); // 10.0
+
+    a *= 2.0;
+    System.out.println(a); // 20.0
+
+    a /= 2.0;
+    System.out.println(a); // 10.0
+
+    a %= 2.0;
+    System.out.println(a); // 0.0
+
+    // Unary Operations
+    a = 10.0;
+    c = +a;
+    System.out.println(c); // 10.0
+
+    c = -a;
+    System.out.println(c); // -10.0
+  }
 }
--- contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/PrimitiveFloatOperations.java
+++ contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/PrimitiveFloatOperations.java
@@ -1,5 +1,60 @@
 package com.ayokoding.cookbook.data_types;
 
 public class PrimitiveFloatOperations {
+  public static void main(String[] args) {
+    // Arithmetic Operations
+    float a = 10.5f;
+    float b = 20.5f;
+    float c;
 
+    c = a + b;
+    System.out.println(c); // 31.0
+
+    c = b - a;
+    System.out.println(c); // 10.0
+
+    c = a * b;
+    System.out.println(c); // 215.25
+
+    c = b / a;
+    System.out.println(c); // 1.9523809
+
+    c = b % a;
+    System.out.println(c); // 10.0
+
+    // Relational Operations
+    System.out.println(a == b); // false
+    System.out.println(a != b); // true
+    System.out.println(a > b); // false
+    System.out.println(a < b); // true
+    System.out.println(a >= b); // false
+    System.out.println(a <= b); // true
+
+    // Assignment Operations
+    a = 10.5f;
+    System.out.println(a); // 10.5
+
+    a += 2.5f;
+    System.out.println(a); // 13.0
+
+    a -= 2.5f;
+    System.out.println(a); // 10.5
+
+    a *= 2.5f;
+    System.out.println(a); // 26.25
+
+    a /= 2.5f;
+    System.out.println(a); // 10.5
+
+    a %= 2.5f;
+    System.out.println(a); // 0.5
+
+    // Unary Operations
+    a = 10.5f;
+    c = +a;
+    System.out.println(c); // 10.5
+
+    c = -a;
+    System.out.println(c); // -10.5
+  }
 }
