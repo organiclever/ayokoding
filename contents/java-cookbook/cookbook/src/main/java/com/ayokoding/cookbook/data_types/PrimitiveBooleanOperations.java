854012a4eadd8552c08588eee701ccb7d1c285b8
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-08 07:18:55 +0700

    [java-cookbook] add primite char and boolean operations

--- contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/PrimitiveBooleanOperations.java
+++ contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/PrimitiveBooleanOperations.java
@@ -1,5 +1,45 @@
 package com.ayokoding.cookbook.data_types;
 
 public class PrimitiveBooleanOperations {
+  public static void main(String[] args) {
 
+    // boolean values
+    boolean bool1 = true;
+    boolean bool2 = false;
+
+    // Logical AND
+    boolean result = bool1 && bool2;
+    System.out.println(result); // false
+
+    // Logical OR
+    result = bool1 || bool2;
+    System.out.println(result); // true
+
+    // Logical XOR (exclusive OR)
+    result = bool1 ^ bool2;
+    System.out.println(result); // true
+
+    // Logical NOT
+    result = !bool1;
+    System.out.println(result); // false
+
+    // Equality Check
+    result = (bool1 == bool2);
+    System.out.println(result); // false
+
+    // Inequality Check
+    result = (bool1 != bool2);
+    System.out.println(result); // true
+
+    // Assignment
+    bool1 = false;
+    System.out.println(bool1); // false
+
+    bool1 = true;
+    System.out.println(bool1); // true
+
+    // Conditional (Ternary) Operator
+    result = (bool1 == bool2) ? bool1 : bool2;
+    System.out.println(result); // false
+  }
 }
--- contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/PrimitiveCharOperations.java
+++ contents/java-cookbook/cookbook/src/main/java/com/ayokoding/cookbook/data_types/PrimitiveCharOperations.java
@@ -1,5 +1,109 @@
 package com.ayokoding.cookbook.data_types;
 
 public class PrimitiveCharOperations {
+  public static void main(String[] args) {
+    // Arithmetic Operations
+    char a = 'A';
+    char b = 'B';
+    int c;
 
+    c = a + b;
+    System.out.println(c); // 131
+
+    c = b - a;
+    System.out.println(c); // 1
+
+    c = a * b;
+    System.out.println(c); // 4290
+
+    c = b / a;
+    System.out.println(c); // 1
+
+    c = b % a;
+    System.out.println(c); // 1
+
+    // Relational Operations
+    System.out.println(a == b); // false
+    System.out.println(a != b); // true
+    System.out.println(a > b); // false
+    System.out.println(a < b); // true
+    System.out.println(a >= b); // false
+    System.out.println(a <= b); // true
+
+    // Bitwise Operations
+    c = a & b;
+    System.out.println(c); // 64
+
+    c = a | b;
+    System.out.println(c); // 67
+
+    c = a ^ b;
+    System.out.println(c); // 3
+
+    c = ~a;
+    System.out.println(c); // -66
+
+    c = a << 2;
+    System.out.println(c); // 260
+
+    c = a >> 2;
+    System.out.println(c); // 16
+
+    c = a >>> 2;
+    System.out.println(c); // 16
+
+    // Assignment Operations
+    a = 'A';
+    System.out.println(a); // A
+
+    a += 2;
+    System.out.println(a); // C
+
+    a -= 2;
+    System.out.println(a); // A
+
+    a *= 2;
+    System.out.println(a); // Ä
+
+    a /= 2;
+    System.out.println(a); // A
+
+    a %= 2;
+    System.out.println(a); // A
+
+    a = 'A';
+    a &= 'B';
+    System.out.println(a); // @
+
+    a = 'A';
+    a |= 'B';
+    System.out.println(a); // C
+
+    a = 'A';
+    a ^= 'B';
+    System.out.println(a); // C ?
+
+    a = 'A';
+    a <<= 2;
+    System.out.println(a); // Ä
+
+    a = 'Ä';
+    a >>= 2;
+    System.out.println(a); // 1
+
+    a = 'Ä';
+    a >>>= 2;
+    System.out.println(a); // 1
+
+    // Unary Operations
+    a = 'A';
+    c = +a;
+    System.out.println(c); // 65
+
+    a++;
+    System.out.println(a); // B
+
+    a--;
+    System.out.println(a); // A
+  }
 }
