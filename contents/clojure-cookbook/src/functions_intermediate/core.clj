d42477a61632ac0b41b7d9d69b69827e5e930372
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-09 21:20:13 +0700

    [clojure-cookbook] update clojure cookbook

--- contents/clojure-cookbook/src/functions_intermediate/core.clj
+++ contents/clojure-cookbook/src/functions_intermediate/core.clj
@@ -1,8 +1,43 @@
 (ns functions-intermediate.core)
 
-;; Higher-order functions
-;; Anonymous functions
-;; Recursion
+;; Higher-order function
+(defn apply-twice [f x]
+  (f (f x)))
+(defn square [x]
+  (* x x))
+
+(apply-twice square 2)
+;; => 16
+
+;; Anonymous function
+(fn [x] (* x 2))
+((fn [x] (* x 2)) 5)
+;; => 10
+
+;; Recursive function
+(defn factorial [n]
+  (if (<= n 1)
+    1
+    (* n (factorial (dec n)))))
+(factorial 5)
+;; => 120
+
 ;; Function composition
-;; Partial application
-;; Currying
+(defn add-one [x]
+  (+ x 1))
+(defn triple [x]
+  (* x 3))
+(defn cubic [x]
+  (* x x x))
+((comp add-one triple cubic) 3)
+;; => 82
+
+;; Partial application using the `partial` function
+((partial * 3) 4)
+;; => 12
+
+(defn add [x y]
+  (+ x y))
+(def curried-add (partial add 5))
+(curried-add 3)
+;; => 8
