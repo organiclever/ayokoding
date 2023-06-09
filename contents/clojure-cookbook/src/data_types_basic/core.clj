4f8528c8564ee678b9abc3199eb5574ed1726765
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-09 16:10:49 +0700

    [clojure-cookbook] update clojure cookbook

--- contents/clojure-cookbook/src/data_types_basic/core.clj
+++ contents/clojure-cookbook/src/data_types_basic/core.clj
@@ -1,11 +1,21 @@
 (ns data-types-basic.core)
 
+;; ---
+;; Basic data types
+;; ---
+
 ;; Integers
 42
 ;; => 42
 ;; Float
 3.14
 ;; => 3.14
+36786883868216818816N
+;; => 36786883868216818816N
+3.14159265358M
+;; => 3.14159265358M
+22/7
+;; => 22/7
 
 ;; Booleans
 true
@@ -30,4 +40,41 @@ false
 ;; => :my-keyword
 
 nil
+;; => nil
+
+;; ---
+;; Underlying Class
+;; ---
+
+(class 42)
+;; => java.lang.Long
+
+(class 3.14)
+;; => java.lang.Double
+
+(class 36786883868216818816N)
+;; => clojure.lang.BigInt
+
+(class 3.14159265358M)
+;; => java.math.BigDecimal
+
+(class 22/7)
+;; => clojure.lang.Ratio
+
+(class true)
+;; => java.lang.Boolean
+
+(class false)
+;; => java.lang.Boolean
+
+(class \a)
+;; => java.lang.Character
+
+(class "abcdef123456")
+;; => java.lang.String
+
+(class :my-keyword)
+;; => clojure.lang.Keyword
+
+(class nil)
 ;; => nil
