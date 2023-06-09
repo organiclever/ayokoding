5563071ddaa7cecf39c49c51414dc4cdc1ea5728
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-10 04:40:59 +0700

    [clojure-cookbook] update clojure cookbook

--- contents/clojure-cookbook/src/functions_intermediate/core.clj
+++ contents/clojure-cookbook/src/functions_intermediate/core.clj
@@ -1,4 +1,5 @@
-(ns functions-intermediate.core)
+(ns functions-intermediate.core
+  (:require [clojure.repl :refer [doc]]))
 
 ;; ---
 ;; Playing with Function
@@ -94,3 +95,29 @@
       (recur b (+ a b) (dec n)))))
 (fibonacci-with-recur 10)
 ;; => 55
+
+;; ---
+;; Other utilities
+;; ---
+
+(doc partial)
+;; => print:
+;; (doc partial)
+;; -------------------------
+;; clojure.core/partial
+;; ([f] [f arg1] [f arg1 arg2] [f arg1 arg2 arg3] [f arg1 arg2 arg3 & more])
+;; Takes a function f and fewer than the normal arguments to f, and
+;; returns a fn that takes a variable number of additional args. When
+;; called, the returned function calls f with args + additional args.
+
+(defn triple-2
+  "Calculates the tripe of a number."
+  [x]
+  (* x x))
+
+(doc triple-2)
+;; => print:
+;; -------------------------
+;; functions-intermediate.core/triple-2
+;; ([x])
+;; Calculates the tripe of a number.
