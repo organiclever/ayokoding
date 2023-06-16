7ad6b00330833d218a3f48735c9a0f95a35c06c9
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-16 08:55:08 +0700

    [clojure-cookbook] update clojure cookbook - add working with edn files

--- contents/clojure-cookbook/src/files_edn/core.clj
+++ contents/clojure-cookbook/src/files_edn/core.clj
@@ -8,35 +8,35 @@
   (with-open [reader (io/reader file-path)]
     (edn/read-string (slurp reader))))
 
-(def edn_data (read-edn-file "data_set/example.edn"))
+(def edn_simple_sample_data (read-edn-file "data_set/read_simple_example.edn"))
 
-edn_data
+edn_simple_sample_data
 ;; => {:name "John Doe", :age 30, :email "john.doe@example.com", :address {:street "123 Main St", :city "New York", :state "NY", :zip "10001"}, :interests ["programming" "reading" "hiking"]}
 
-(class edn_data)
+(class edn_simple_sample_data)
 ;; => clojure.lang.PersistentArrayMap
 
-(:name edn_data)
+(:name edn_simple_sample_data)
 ;; => "John Doe"
-(:age edn_data)
+(:age edn_simple_sample_data)
 ;; => 30
-(:email edn_data)
+(:email edn_simple_sample_data)
 ; => "john.doe@example.com"
-(:address edn_data)
+(:address edn_simple_sample_data)
 ;; => {:street "123 Main St", :city "New York", :state "NY", :zip "10001"}
-(:street (:address edn_data))
+(:street (:address edn_simple_sample_data))
 ;; => "123 Main St"
-(:interests edn_data)
+(:interests edn_simple_sample_data)
 ;; => ["programming" "reading" "hiking"]
 
-(def edn_meta_data (meta edn_data))
+(def edn_simple_sample_meta_data (meta edn_simple_sample_data))
 
-edn_meta_data
+edn_simple_sample_meta_data
 ;; => {:author "John", :created-at "2022-01-01"}
 
-(:author edn_meta_data)
+(:author edn_simple_sample_meta_data)
 ;; => "John"
-(:created-at edn_meta_data)
+(:created-at edn_simple_sample_meta_data)
 ;; => "2022-01-01"
 
 ;; Writing EDN Files
@@ -46,4 +46,4 @@ edn_meta_data
     (binding [*out* writer]
       (prn data))))
 
-(write-edn-file "data_set/example2.edn" edn_data)
+(write-edn-file "data_set/write_simple_example.edn" edn_simple_sample_data)
