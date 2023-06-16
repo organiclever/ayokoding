8967b9141c95c0f6cffd8db9da6c92d4a8994883
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-16 14:50:10 +0700

    [clojure-cookbook] update clojure cookbook - add working with java

---
+++ contents/clojure-cookbook/src/interop_java_in_clojure/Calculator.java
@@ -0,0 +1,7 @@
+package interop_java_in_clojure;
+
+public class Calculator {
+  public int superAdd(int a, int b) {
+    return a + b;
+  }
+}
--- contents/clojure-cookbook/src/interop_java_in_clojure/core.clj
+++ contents/clojure-cookbook/src/interop_java_in_clojure/core.clj
@@ -1,8 +1,10 @@
-(ns interop-java-in-clojure.core)
+(ns interop-java-in-clojure.core
+  (:require [clojure.pprint :as pprint]
+            [clojure.reflect :as reflect]))
 
-;; -----
+;; ---
 ;; Interop basics
-;; -----
+;; ---
 
 ;; Call instance method
 
@@ -39,3 +41,68 @@ dim
 (import 'java.util.UUID)
 (UUID/randomUUID)
 ;; => #uuid "467c1c23-5a96-4e09-bcbe-497abcb6d73f"
+
+;; ---
+;; Java data types
+;; ---
+
+(int 42)
+;; => 42
+(double 3.14)
+;; => 3.14
+(+ (int 42) (double 3.14))
+;; => 45.14
+
+(def java-array (to-array [1 2 3]))
+(class java-array)
+;; => [Ljava.lang.Object;
+(seq java-array)
+;; => (1 2 3)
+
+(def java-string (String. "Hello"))
+;; => #'interop-java-in-clojure.core/java-string
+(class java-string)
+;; => java.lang.String
+(.length java-string)
+;; => 5
+(str java-string " World")
+;; => "Hello World"
+
+;; ---
+;; exception handling
+;; ---
+
+(try (/ (int 42) (int 0))
+     (catch ArithmeticException e
+       (str "Exception name: " e " >>> Message: "  (.getMessage e))))
+;; => "Exception name: java.lang.ArithmeticException: Divide by zero >>> Message: Divide by zero"
+
+;; ---
+;; Java external libraries
+;; ---
+
+(import 'com.google.common.base.Strings)
+(Strings/repeat "Hello" 3)
+;; => "HelloHelloHello"
+(Strings/titleCase "hello world")
+
+(import 'com.google.common.base.Preconditions)
+
+(defn divide [numerator denominator]
+  (Preconditions/checkArgument (not= denominator 0) "Denominator must be non-zero")
+  (/ numerator denominator))
+
+(try
+  (divide 42 0)
+  (catch IllegalArgumentException e
+    (str "Exception name: " e " >>> Message: "  (.getMessage e))))
+;; => "Exception name: java.lang.IllegalArgumentException: Denominator must be non-zero >>> Message: Denominator must be non-zero"
+
+(keys (reflect/reflect Strings))
+;; => (:bases :flags :members)
+
+;; ---
+;; Local Java file
+;; ---
+
+;; (import Calculator)
