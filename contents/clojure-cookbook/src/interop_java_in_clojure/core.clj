1683a2944ef76f6be97f254560fa427b7572eae2
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-16 13:58:35 +0700

    [clojure-cookbook] update clojure cookbook - add working with java

--- contents/clojure-cookbook/src/interop_java_in_clojure/core.clj
+++ contents/clojure-cookbook/src/interop_java_in_clojure/core.clj
@@ -1,11 +1,41 @@
 (ns interop-java-in-clojure.core)
 
+;; -----
 ;; Interop basics
+;; -----
 
-;; Java data types in clojure
+;; Call instance method
 
-;; Java method invocation
+(import 'java.util.ArrayList)
 
-;; Java object creation
+(def my-list (ArrayList.))
+my-list
+;; => []
+(.add my-list "Hello")
+;; => true
+my-list
+;; => ["Hello"]
+(.add my-list "World")
+;; => true
+my-list
+;; => ["Hello" "World"]
+(.size my-list)
+;; => 2
 
+;; Call fields
 
+(import 'java.awt.Dimension)
+
+(def dim (Dimension. 10 20))
+dim
+;; => #object[java.awt.Dimension 0x48d8fc0 "java.awt.Dimension[width=10,height=20]"]
+(.width dim)
+;; => 10
+(.height dim)
+;; => 20
+
+;; Call static method
+
+(import 'java.util.UUID)
+(UUID/randomUUID)
+;; => #uuid "467c1c23-5a96-4e09-bcbe-497abcb6d73f"
