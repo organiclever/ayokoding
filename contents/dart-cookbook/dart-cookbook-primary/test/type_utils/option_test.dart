ed25109af9b94818df5078fe609fdae701624869
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-30 21:38:10 +0700

    [cookbook] update

--- contents/dart-cookbook/dart-cookbook-primary/test/type_utils/option_test.dart
+++ contents/dart-cookbook/dart-cookbook-primary/test/type_utils/option_test.dart
@@ -1,1 +1,16 @@
+import 'package:test/test.dart';
+import "../../type_utils/option.dart";
 
+void main() {
+  group('Option creation for none', () {
+    test('None will return default value when getOrElse', () {
+      expect(None().isEqual(None()), equals(true));
+    });
+    test('None is None', () {
+      expect(None().isNone(), equals(true));
+    });
+    test('None is not Some', () {
+      expect(None().isSome(), equals(false));
+    });
+  });
+}
