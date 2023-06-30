05554d70af5df161f39ec4d464f5b776f39b7727
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-06-30 14:21:36 +0700

    [cookbook] update

---
+++ contents/dart-cookbook/dart_official/patterns/patterns_demo.dart
@@ -0,0 +1,72 @@
+sealed class Result<T, U> {
+  map<T, T1, U>(T1 Function(T) f) {
+    switch (this) {
+      case (Ok ok):
+        return Ok(f(ok.value));
+      case (Error error):
+        return Error(error.value);
+    }
+  }
+
+  mapError<T, U, U1>(U1 Function(U) f) {
+    switch (this) {
+      case (Ok ok):
+        return Ok(ok.value);
+      case (Error error):
+        return Error(f(error.value));
+    }
+  }
+
+  T getOrElse(T defVal) {
+    switch (this) {
+      case (Ok ok):
+        return ok.value;
+      case (Error _):
+        return defVal;
+    }
+  }
+}
+
+class Ok<T> extends Result {
+  final T value;
+
+  Ok(this.value);
+}
+
+class Error<U> extends Result {
+  final U value;
+
+  Error(this.value);
+}
+
+String processResult(Result res) {
+  switch (res) {
+    case (Ok ok):
+      return "OK: ${ok.value}";
+    case (Error error):
+      return "Error: ${error.value}";
+  }
+}
+
+void main() {
+  print(processResult(Ok("Hello World"))); // OK: Hello World
+  print(processResult(Ok(100))); // Ok: 100
+  print(processResult(Error("Error Bro!"))); // Error: Error Bro!
+  print(processResult(Error(-100))); // Error: -100
+
+  print("---");
+
+  print(Ok(100).getOrElse(0));
+  print(Error(100).getOrElse(0));
+
+  var theVal = Ok(100).map((String val) => val * 2).getOrElse(0);
+  print(theVal);
+
+  print("---");
+
+  var mapped = Ok(10).map((message) => message.toString());
+  var errorMapped = Ok(10).mapError((message) => message.toString());
+
+  print(processResult(mapped));
+  print(processResult(errorMapped));
+}
