9673aa13302906447fb139d62673bcd8abfe2955
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-07-10 21:26:01 +0700

    [cookbook] update

---
+++ contents/dart-cookbook/dart-cookbook-primary/src/typing/exception_utils.dart
@@ -0,0 +1,29 @@
+import 'result.dart';
+
+Result<T, ({Object ex, StackTrace st})> tryCatch<T>(T Function() operation) {
+  try {
+    T result = operation();
+    return Ok(result);
+  } catch (e, s) {
+    return Error((ex: e, st: s));
+  }
+}
+
+void main() {
+  // this will wrap exception in Result type
+  var res = tryCatch<bool>(() {
+    dynamic foo = true;
+    return foo++;
+  });
+
+  print(res.isError()); // Output: true
+  print(res.isOk()); // Output: false
+
+  res.tapError((err) {
+    print(err.ex.runtimeType);
+    print(err.st.runtimeType);
+  });
+  // Output:
+  // NoSuchMethodError
+  // _StackTrace
+}
