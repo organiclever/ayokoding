80800d69ea68395c703daf0cb3686b8d70987ca0
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-07-15 17:34:06 +0700

    [cookbook] update

--- contents/clojure-cookbook/clojure-cookbook-primary/src/labs/concurrency/c_04_atom/core.clj
+++ contents/clojure-cookbook/clojure-cookbook-primary/src/labs/concurrency/c_04_atom/core.clj
@@ -1,1 +1,26 @@
 (ns labs.concurrency.c-04-atom.core)
+
+;; create an Atom with initially no money
+(def donation-count (atom 0))
+
+;; start 9 people collecting money (9 threads)
+(dotimes [_ 9]
+  (doto (Thread. (fn []
+                   ;; wait three seconds
+                   (Thread/sleep 1000)
+                   ;; go collect $1
+                   (swap! donation-count inc)
+                   ;; do it again
+                   ))
+    .start))
+
+;; start one person tweeting
+(doto (Thread. (fn []
+                 ;; wait 100 seconds
+                 (Thread/sleep 10000)
+                 (println (str "We collected $" @donation-count " total!"))))
+  .start)
+;; => We collected $ 9 total!
+
+@donation-count
+;; => 9
