46ba808c9770e254ea88f53d7ec5b989c09e2af0
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-07-15 14:12:56 +0700

    [cookbook] update

---
+++ contents/clojure-cookbook/clojure-cookbook-primary/src/labs/concurrency/core.clj
@@ -0,0 +1,40 @@
+(ns labs.concurrency.core)
+
+;; This module is a follow along of Eric Normand's Clojure concurrency
+;; https://ericnormand.me/guide/clojure-concurrency
+
+;; Delay - I may not need to calculate this
+
+;; Promise - I'll check back here for the answer
+
+;; Future - Please calculate this in another thread
+
+;; A word about deref - What's in the box?
+
+;; Atom - Keeping a single value consistent over time
+
+;; Ref - Keep multiple values in a consistent relationship
+
+;; Var - Redefine stuff as you program
+
+;; Agent - Queue up work to aggregate a value
+
+;; Volatile - Lightweight mutable state
+
+;; Lock - Keep others out while you're in
+
+;; core.async - Workers with conveyor belts
+
+;; Threads - Use your cores
+
+;; Executor Service - Run lots of tasks in a thread pool
+
+;; Manifold - Abstractions for data sources
+
+;; pmap - Simple parallelism with one more letter
+
+;; Reducers - Transformation pipelines in parallel
+
+;; Fork/Join - Break your work into chunks, do the chunks on your cores, put them back together
+
+
