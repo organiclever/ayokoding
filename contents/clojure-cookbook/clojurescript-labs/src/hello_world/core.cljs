d83c91ddf5eaaf455ee85fb519c34781e7345c8f
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-07-16 21:16:09 +0700

    update

--- contents/clojure-cookbook/clojurescript-labs/src/hello_world/core.cljs
+++ contents/clojure-cookbook/clojurescript-labs/src/hello_world/core.cljs
@@ -6,10 +6,22 @@
 
 (def state (atom {:count 0}))
 
-(.render js/ReactDOM
-         (.createElement js/React "h2" nil (str "Hello??? React!!!! " (average 20 (:count @state))))
-         (.getElementById js/document "app"))
+(defn render []
+  (.render js/ReactDOM
+           (.createElement js/React "h2" nil (str "Hello??? React!!!! \n count: " (:count @state)))
+           (.getElementById js/document "app")))
+
+(defn print-change [_ _ _ new-value]
+  (println (str "The new value is: " new-value))
+  (render)
+  (js/console.log "The new value is:" new-value))
+
+(add-watch state :main-state print-change)
 
 (swap! state update :count inc)
 
 (js/console.log (:count @state))
+
+(comment
+  (swap! state (fn [_]
+                 {:count 0})))
