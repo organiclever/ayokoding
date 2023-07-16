d6bdbd361ead646a5d2d019c288d8f2802b276ae
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-07-16 09:50:27 +0700

    [cookbook] update

--- contents/clojure-cookbook/clojurescript-labs/src/hello_world/core.cljs
+++ contents/clojure-cookbook/clojurescript-labs/src/hello_world/core.cljs
@@ -5,7 +5,7 @@
   (/ (+ x y) 2))
 
 (.render js/ReactDOM
-         (.createElement js/React "h2" nil (str "Hello??? React! " (average 20 20)))
+         (.createElement js/React "h2" nil (str "Hello??? React!!! " (average 20 20)))
          (.getElementById js/document "app"))
 
 (js/console.log {:hello "world"})
