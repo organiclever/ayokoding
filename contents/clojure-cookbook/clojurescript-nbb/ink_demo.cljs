7be09133548faca12045c033fcd87eb2d1fe1493
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-07-16 09:47:43 +0700

    [cookbook] update

---
+++ contents/clojure-cookbook/clojurescript-nbb/ink_demo.cljs
@@ -0,0 +1,13 @@
+(ns ink-demo
+  (:require ["ink" :refer [render Text]]
+            [reagent.core :as r]))
+
+(defonce state (r/atom 0))
+
+(doseq [n (range 1 1000)]
+  (js/setTimeout #(swap! state inc) (* n 500)))
+
+(defn hello []
+  [:> Text {:color "green"} "Hello, world! " @state])
+
+(render (r/as-element [hello]))
