61e4c9f0d4012706a78775fc9c0dde1cecac4e0b
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-07-18 19:09:31 +0700

    [cookbook] update

--- contents/clojure-cookbook/clojurescript-labs/src/hello_world/core.cljs
+++ contents/clojure-cookbook/clojurescript-labs/src/hello_world/core.cljs
@@ -3,7 +3,8 @@
 (def state (atom {:count 0}))
 
 (defn render [app-state]
-  (set! (.-innerHTML (.getElementById js/document "app")) (str "Hello! \n count: " (:count app-state))))
+  (set! (.-innerHTML (.getElementById js/document "app"))
+        (str "Hello! \n count: " (:count app-state))))
 
 (defn handle-state-change [_ _ _ new-state]
   (render new-state))
@@ -12,9 +13,7 @@
 
 (swap! state update :count inc)
 
-(.getElementById js/document "app")
-(set! (.-innerHTML (.getElementById js/document "app")) (str "Hello! \n count: "))
-
 (comment
   (swap! state (fn [_]
-                 {:count 0})))
+                 {:count 0}))
+  (js/alert "hello"))
