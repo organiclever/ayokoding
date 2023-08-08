d8f8b1b6bf5aaed023707756158eff14f8820ffc
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-08-09 02:45:34 +0700

    add pedestal hello world

---
+++ contents/clojure-cookbook/clojure-cookbook-primary/src/http_server_pedestal/hello.clj
@@ -0,0 +1,27 @@
+(ns http-server-pedestal.hello
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+(defn respond-hello [_request]
+  {:status 200
+   :body "Hello, World!"})
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]}))
+
+(route/try-routing-for routes :prefix-tree "/greet" :get)
+(route/try-routing-for routes :prefix-tree "/greet" :post)
+(route/try-routing-for routes :prefix-tree "/greet" :put)
+
+(defn create-server []
+  (http/create-server
+   {::http/routes routes
+    ::http/type :jetty
+    ::http/port 8890}))
+
+(defn start []
+  (http/start (create-server)))
+
+(comment
+  (start))
