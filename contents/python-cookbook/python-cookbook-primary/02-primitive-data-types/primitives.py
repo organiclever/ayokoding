405295d0811f76a274f4b062e2a80f35115fbe8f
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-04-16 17:10:36 +0700

    feat(python-cookbook-primary): primitives data type

---
+++ contents/python-cookbook/python-cookbook-primary/02-primitive-data-types/primitives.py
@@ -0,0 +1,47 @@
+# primitive data types
+
+# - int
+
+age = 25
+quantity = -10
+
+print("age: ", age, ", type: ", type(age))
+print("quantity: ", quantity, ", type: ", type(quantity))
+
+print("---")
+# - float
+
+price = 9.99
+pi = 3.14159
+
+print("price: ", price, ", type: ", type(price))
+print("pi: ", pi, ", type: ", type(pi))
+
+print("---")
+# - complex
+
+z = 3 + 4j
+print("z: ", z, ", type: ", type(z))
+
+print("---")
+# - boolean
+
+true_logic = True
+false_logic = False
+
+print("true_logic: ", true_logic, ", type: ", type(true_logic))
+print("false_logic: ", false_logic, ", type: ", type(false_logic))
+
+print("---")
+# - string
+
+name = "John Doe"
+
+print("name: ", name, ", type: ", type(name))
+
+print("---")
+# NoneType
+
+result = None
+
+print("result: ", result, ", type: ", type(result))
