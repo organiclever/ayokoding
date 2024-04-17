23f7a5ba4566bff1f7d200c4af4bf5e53b4c01c2
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-04-17 07:39:35 +0700

    feat(python-cookbook-primary): fill float-operations

---
+++ contents/python-cookbook/python-cookbook-primary/02-data-types-primitives.py/float-operations.py
@@ -0,0 +1,93 @@
+# arithmetic operations
+
+print("--- arithmetic operations ---")
+
+a = 3.4
+b = 1.2
+
+# Addition
+result = a + b
+print(result)  # Output: 4.6
+
+# Subtraction
+result = a - b
+print(result)  # Output: 2.2
+
+# Multiplication
+result = a * b
+print(result)  # Output: 4.08
+
+# Division
+result = a / b
+print(result)  # Output: 2.8333333333333335
+
+# Modulus
+result = a % b
+print(result)  # Output: 1.0
+
+# Exponentiation
+result = a ** b
+print(result)  # Output: 4.342848711597634
+
+# Floor Division
+result = a // b
+print(result)  # Output: 2.0
+
+# assignment operations
+
+print("--- assignment operations ---")
+
+a = 3.4
+
+# Addition assignment
+a += 1.2  # Equivalent to: a = a + 1.2
+print(a)  # Output: 4.6
+
+# Subtraction assignment
+a -= 0.8  # Equivalent to: a = a - 0.8
+print(a)  # Output: 3.8
+
+# Multiplication assignment
+a *= 2.5  # Equivalent to: a = a * 2.5
+print(a)  # Output: 9.5
+
+# Division assignment
+a /= 3.0  # Equivalent to: a = a / 3.0
+print(a)  # Output: 3.1666666666666665
+
+# Modulus assignment
+a %= 2.0  # Equivalent to: a = a % 2.0
+print(a)  # Output: 1.1666666666666665
+
+# Exponentiation assignment
+a **= 2.0  # Equivalent to: a = a ** 2.0
+print(a)  # Output: 1.3611111111111107
+
+# Floor division assignment
+a //= 0.5  # Equivalent to: a = a // 0.5
+print(a)  # Output: 2.0
+
+# comparison operations
+
+print("--- comparison operations ---")
+
+a = 3.4
+b = 5.7
+
+# Equal to
+print(a == b)  # Output: False
+
+# Not equal to
+print(a != b)  # Output: True
+
+# Greater than
+print(a > b)  # Output: False
+
+# Less than
+print(a < b)  # Output: True
+
+# Greater than or equal to
+print(a >= b)  # Output: False
+
+# Less than or equal to
+print(a <= b)  # Output: True
