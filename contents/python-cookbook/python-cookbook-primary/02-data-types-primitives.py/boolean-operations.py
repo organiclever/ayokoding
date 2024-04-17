2ea3d3a0f76adf772fb87a781d343b1c24c550a6
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-04-17 07:49:34 +0700

    feat(python-cookbook-primary): fill boolean-operations

---
+++ contents/python-cookbook/python-cookbook-primary/02-data-types-primitives.py/boolean-operations.py
@@ -0,0 +1,13 @@
+a = True
+b = False
+result = a and b
+print(result)  # Output: False
+
+a = True
+b = False
+result = a or b
+print(result)  # Output: True
+
+a = True
+result = not a
+print(result)  # Output: False
