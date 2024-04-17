22167864b6f3a5274fcb621ad9372352b00b4c9a
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-04-17 08:56:26 +0700

    feat(python-cookbook-primary): fill string-operations

--- contents/python-cookbook/python-cookbook-primary/02-data-types-primitives.py/string-operations.py
+++ contents/python-cookbook/python-cookbook-primary/02-data-types-primitives.py/string-operations.py
@@ -40,9 +40,66 @@ str1 = "Hello, Python!"
 lowercase = str1.lower()
 print(lowercase)  # Output: hello, python!
 
-# formatting using %
+# single line formatting
+
+# - formatting using %
 
 name = "Alice"
 age = 25
 message = "My name is %s and I am %d years old." % (name, age)
 print(message)  # Output: My name is Alice and I am 25 years old.
+
+# - using str.format()
+
+name = "Bob"
+age = 30
+message = "My name is {} and I am {} years old.".format(name, age)
+print(message)  # Output: My name is Bob and I am 30 years old.
+
+# - using f-strings
+
+name = "Charlie"
+age = 35
+message = f"My name is {name} and I am {age} years old."
+print(message)  # Output: My name is Charlie and I am 35 years old.
+
+# multiline formatting
+
+# - formatting using %
+
+name = "Alice"
+age = 25
+
+message = """
+Hello,
+My name is %s.
+I am %d years old.
+""" % (name, age)
+
+print(message)
+
+# - formatting using str.format()
+
+name = "Alice"
+age = 25
+
+message = """
+Hello,
+My name is {}.
+I am {} years old.
+""".format(name, age)
+
+print(message)
+
+# - formatting using f-strings
+
+name = "Alice"
+age = 25
+
+message = f"""
+Hello,
+My name is {name}.
+I am {age} years old.
+"""
+
+print(message)
